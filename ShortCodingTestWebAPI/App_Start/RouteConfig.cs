963b91f7377439b870d00729904efb1c0b930eae
John Amarante <john.amarante8888@gmail.com>
2020-01-13 12:25:29 -0800

    put API link on the default page

--- ShortCodingTestWebAPI/App_Start/RouteConfig.cs
+++ ShortCodingTestWebAPI/App_Start/RouteConfig.cs
@@ -16,6 +16,7 @@ namespace ShortCodingTestWebAPI
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
+                //defaults: new { controller = "Help", action = "Index", id  = UrlParameter.Optional }
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
